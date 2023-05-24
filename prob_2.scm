7aa11e28603b620fc4742950007d2fe1155dbe4e
alexdesiqueira <alex.desiqueira@igdore.org>
2023-05-24 17:30:12 -0300

    Add fixed solution to Problem 2

--- prob_2.scm
+++ prob_2.scm
@@ -1,32 +1,13 @@
 (define (sum-even-fibonacci-numbers-up-to number)
-  (sum-iter 0 1 number))
+  (define (fibonacci-sum-helper a b sum)
+    (if (> b number)
+        sum
+        (if (even? b)
+            (fibonacci-sum-helper b (+ a b) (+ sum b))
+            (fibonacci-sum-helper b (+ a b) sum))))
 
+  (fibonacci-sum-helper 1 1 0))
 
-;; broken. need to check
-(define (sum-iter sum curr_fibo number)
-  (display sum)
-  (newline)
-  (display curr_fibo)
-  (newline)
-  (display number)
-  (newline)
-  (newline)
-  (if (and (<= (fibonacci curr_fibo) number) (even? curr_fibo))
-      (sum-iter (+ sum curr_fibo) (fibonacci (+ curr_fibo 1)) number)
-      (sum-iter sum (fibonacci (+ curr_fibo 1)) number)))
+(display (sum-even-fibonacci-numbers-up-to 4000000))
+(newline)
 
-
-;; this fibonacci definition is given in SICP, page 39
-;; adapted: fib_1 = 1; fib_2 = 1
-(define (fibonacci number)
-  (fib-iter 1 1 number))
-
-(define (fib-iter a b count)
-  (if (= count 0)
-      b
-      (fib-iter (+ a b) a (- count 1))))
-
-
-;; tests
-;;(display (fibonacci 10))
-(display (sum-even-fibonacci-numbers-up-to 11))
