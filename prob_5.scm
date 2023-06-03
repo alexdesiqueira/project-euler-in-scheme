d8dfd7bafe91a9360d3746db7d475a926ec144dd
alexdesiqueira <alex.desiqueira@igdore.org>
2023-06-03 14:16:08 -0300

    Add solution to Problem 5 -- start from 11, only search increments of 2520

--- prob_5.scm
+++ prob_5.scm
@@ -4,15 +4,20 @@
       #f))
 
 (define (divide-without-remainder-interval number begin end)
-  (define (interval-helper count end)
-    (display (list number count begin end))
-    (newline)
-    (if (>= count end)
-        #t
-        (if (divide-without-remainder number count)
-            (divide-without-remainder-interval number (+ 1 count) end)
-            #f)))
-  (interval-helper begin end))
+  (display (list number begin end))
+  (newline)
+  (cond
+   ((>= begin end) #t)
+   ((equal? (divide-without-remainder number begin) #t)
+    (divide-without-remainder-interval number (+ 1 begin) end))
+   (else #f)))
+
+(define (smallest-positive-evenly-divisible? begin end)
+  (define (smallest-positive-helper guess)
+    (if (equal? (divide-without-remainder-interval guess begin end) #t)
+        guess
+        (smallest-positive-helper (+ 2520 guess))))
+  (smallest-positive-helper 2520))   ;; I mean, it should be higher than 2520 :)
 
 ;; tests
 (display (divide-without-remainder 4 2))  ;; should be #t
@@ -22,3 +27,9 @@
 (display (divide-without-remainder-interval 2520 1 10))  ;; should be #t
 (newline)
 (display (divide-without-remainder-interval 250 1 10))  ;; should be #f
+(newline)
+(display (smallest-positive-evenly-divisible? 1 10))  ;; should be 2520
+(newline)
+
+;; result
+(display (smallest-positive-evenly-divisible? 11 20))
